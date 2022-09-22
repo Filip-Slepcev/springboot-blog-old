80925b9fbf60c15d574ce38acf24e51a01255341
Filip <filipslepcev@yahoo.ca>
2022-09-22 13:13:21 -0400

    uploading project

---
+++ drunk-knight/src/main/java/com/appsdeveloperblog/app/ws/DrunkKnightApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkKnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunkKnightApplication.class, args);
+	}
+
+}
